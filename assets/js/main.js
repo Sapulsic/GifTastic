4e74daeddf59d575de13bcaca0ac77d5b4c4f984
Sapulsic <carlos.barahona1995@gmail.com>
2018-09-24 21:51:31 -0500

    base files

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+// Global Variables
+//  ============================================
+// Arrays and Variables for initiating Data
+
+
+// Functions
+//  ============================================
+// $(document).ready(function (){
+    
+
+
+    // Testing / Debugging
+    // console.log(answerNum);
+    
+    // Main Process
+    //  ============================================
+
+// });
