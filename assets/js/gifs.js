64b1ec968f06f177f8e4474788836fed0cd4c566
Sapulsic <carlos.barahona1995@gmail.com>
2018-09-27 16:35:26 -0500

    made changes

--- assets/js/gifs.js
+++ assets/js/gifs.js
@@ -4,13 +4,13 @@
 
 let search = ['Penguins', 'Elephants', 'Dolphins'];
 let searches = $('#searches');
-let addedButtons = $('addedButtons');
+let addedButtons = $('#addedButtons');
 
 
 
 // Functions
 //  ============================================
-$(document).ready(function (){
+// $(document).ready(function (){
     
     // window.oncontextmenu = function(event) {
     //     event.preventDefault();
@@ -19,30 +19,32 @@ $(document).ready(function (){
     // }
 
     function init() {
-        debugger;
-        $('searchBody').empty();
+        // debugger;1`
+        addedButtons.empty();
         for (let i = 0; i < search.length; i++) {
             let addedGif = $('<button>');
             addedGif.addClass("gifButton")
             addedGif.attr("data-animal", search[i])
             addedGif.text(search[i]);
-            $('#addedButtons').append(addedGif);
+            addedButtons.append(addedGif);
         }
     };
 
     $('#submitButton').on('click', function(e){
-        debugger;
+        // debugger;
         e.preventDefault();
         let newSearch = $('#searchInput').val().trim();
         search.push(newSearch);
+        $('#searchForm')[0].reset();
+        init()
     });
     
 
-    $("#addedButtons").on('click', function() {
-        $('searches').empty();
-        let animal = $(this).data("animal");
+    addedButtons.on('click', function() {
+        // debugger;
+        let animal = $(this).attr("data-animal");
         let queryURL = 'https://api.giphy.com/v1/gifs/search?&q=' + animal + '&limit=100&api_key=xiPtoJ3FAubesUNvmUFWvCSa2k2KWNCw';
-    
+        
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -51,7 +53,8 @@ $(document).ready(function (){
         .then(function(response) {
             console.log(queryURL);
             console.log(response);
-
+            
+            searches.empty();   
             let results = response.data;
             for (let i = 0; i < results.length; i++) {
                 let animalDiv = $('<div>')
@@ -75,6 +78,6 @@ $(document).ready(function (){
 
         // Main Process
     //  ============================================
-    init(search, 'addedButtons', "searches")
+    init()
 
-});
+// });
